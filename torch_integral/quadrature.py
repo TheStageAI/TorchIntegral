6b15f320dc40ec3574c61da14826c9e5ea7ce903
b1n0 <azim4fs@gmail.com>
2023-05-14 23:10:37 +0300

    added indices of integration grids

--- torch_integral/quadrature.py
+++ torch_integral/quadrature.py
@@ -2,9 +2,17 @@ import torch
 
 
 class BaseIntegrationQuadrature(torch.nn.Module):
-    def __init__(self, integration_dims):
+    def __init__(self, integration_dims,
+                 integraion_grid_dims=None):
+
         super().__init__()
         self.integration_dims = integration_dims
+
+        if integraion_grid_dims is None:
+            self.integraion_grid_dims = integration_dims
+        else:
+            self.integraion_grid_dims = integraion_grid_dims
+            assert len(integraion_grid_dims) == len(integration_dims)
 
     def discretize(self, function, grid):
         if callable(function):
@@ -21,18 +29,26 @@ class BaseIntegrationQuadrature(torch.nn.Module):
 
     def forward(self, function, grid):
         discretization = self.discretize(function, grid)
-        discretization = self.multiply_coefficients(discretization, grid)
+        discretization = self.multiply_coefficients(
+            discretization, grid
+        )
 
         return discretization
 
 
 class TrapezoidalQuadrature(BaseIntegrationQuadrature):
-    def __init__(self, integration_dims):
-        super(TrapezoidalQuadrature, self).__init__(integration_dims)
+    def __init__(self, integration_dims,
+                 integraion_grid_dims=None):
+
+        super(TrapezoidalQuadrature, self).__init__(
+            integration_dims, integraion_grid_dims
+        )
 
     def multiply_coefficients(self, discretization, grid):
-        for dim in self.integration_dims:
-            x = grid[dim].to(discretization.device)
+        for i in range(len(self.integration_dims)):
+            grid_i = self.integraion_grid_dims[i]
+            dim = self.integration_dims[i]
+            x = grid[grid_i].to(discretization.device)
             h = torch.zeros_like(x)
             h[1:-1] = x[2:] - x[:-2]
             h[0] = x[1] - x[0]
