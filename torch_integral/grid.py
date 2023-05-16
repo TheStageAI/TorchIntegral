0711d5b0997bce8f2e84231e04555ef7488ac26d
b1n0 <azim4fs@gmail.com>
2023-05-17 01:49:24 +0300

    add size attribute to all grids

--- torch_integral/grid.py
+++ torch_integral/grid.py
@@ -39,6 +39,7 @@ class IGrid(torch.nn.Module):
     def __init__(self):
         super(IGrid, self).__init__()
         self.curr_grid = None
+        self.size = None
 
     def forward(self):
         if self.curr_grid is None:
@@ -53,6 +54,9 @@ class IGrid(torch.nn.Module):
             "Implement this method in derived class."
         )
 
+    def size(self):
+        return self.size
+
     def generate_grid(self):
         raise NotImplementedError(
             "Implement this method in derived class."
@@ -60,13 +64,16 @@ class IGrid(torch.nn.Module):
 
 
 class TrainableGrid1D(IGrid):
-    def __int__(self, size, init_value=None):
-        super(TrainableGrid1D, self).__int__()
+    def __init__(self, size, init_value=None):
+        super(TrainableGrid1D, self).__init__()
 
         if init_value is None:
             self._deltas = torch.nn.Parameter(torch.ones(size - 1))
         else:
             self._deltas = torch.nn.Parameter(init_value)
+
+        self.generate_grid()
+        self.size = size
 
     def ndim(self):
         return 1
@@ -86,33 +93,33 @@ class TrainableGrid1D(IGrid):
 class RandomUniformGrid1D(IGrid):
     def __init__(self, distribution):
         super(RandomUniformGrid1D, self).__init__()
-        self._distribution = distribution
-        self.eval_size = distribution.max_val
+        self.distribution = distribution
+        self.size = distribution.max_val
+        self.generate_grid()
 
     def ndim(self):
         return 1
 
     def generate_grid(self):
         if self.training:
-            size = self._distribution.sample()
+            size = self.distribution.sample()
         else:
-            size = self.eval_size
+            size = self.size
             
         self.curr_grid = torch.linspace(-1, 1, size)
 
         return self.curr_grid
     
     def resize(self, new_size):
-        self.eval_size = new_size
+        self.size = new_size
+        self.generate_grid()
 
 
 class GridND(IGrid):
     def __init__(self, grid_objects_dict):
         super(GridND, self).__init__()
         self.grid_objects = torch.nn.ModuleDict(grid_objects_dict)
-        self.eval()
         self.generate_grid()
-        self.train()
 
     def ndim(self):
         return sum([
@@ -121,8 +128,9 @@ class GridND(IGrid):
 
     def reset_grid(self, dim, new_grid):
         self.grid_objects[str(dim)] = new_grid
+        self.generate_grid()
 
-    def generate_grid(self):
+    def generate_grid(self):  # CHECK AGAIN FORWARD AND GENERATE GRID
         self.curr_grid = [
             self.grid_objects[dim].generate_grid()
             for dim in self.grid_objects
