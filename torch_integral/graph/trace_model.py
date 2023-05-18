38135e9f022b09440d2a8cfecb60a67e14ee084e
b1n0 <azim4fs@gmail.com>
2023-05-19 01:30:59 +0300

    added hooks support

--- torch_integral/graph/trace_model.py
+++ torch_integral/graph/trace_model.py
@@ -1,8 +1,7 @@
 import torch
 from .default_operations import DEFAULT_OPERATIONS
+from .default_operations import DEFAULT_HOOKS
 from .operations import neutral_decorator
-from .operations import neutral_hook
-from ..utils import get_attr_by_name
 from ..utils import remove_all_hooks
 
 
@@ -10,19 +9,15 @@ def replace_operations(module: torch.nn.Module,
                        new_operations=None,
                        skip_modules_list=None) -> torch.nn.Module:
     
-    gm = torch.fx.symbolic_trace(module)
-    graph = gm.graph
-    operations = DEFAULT_OPERATIONS
+    fx_model = torch.fx.symbolic_trace(module)
+    modules = dict(fx_model.named_modules())
+    graph = fx_model.graph
+    operations = DEFAULT_OPERATIONS.copy()
+    hooks_dict = DEFAULT_HOOKS.copy()
+    nodes = list(graph.nodes)
 
     if new_operations is not None:
         operations.update(new_operations)
-
-    if skip_modules_list is not None:
-        skip_modules = (torch.nn.BatchNorm2d, *skip_modules_list)
-    else:
-        skip_modules = (torch.nn.BatchNorm2d,)
-
-    nodes = list(graph.nodes)
 
     for node in nodes:
         if node.op == 'call_function':
@@ -49,16 +44,14 @@ def replace_operations(module: torch.nn.Module,
                 graph.erase_node(node)
 
         elif node.op == 'call_module':
-            node_module = get_attr_by_name(module, node.target)
+            node_module = modules[node.target]
 
-            if isinstance(node_module, skip_modules):
-                node_module.register_forward_hook(neutral_hook)
-            else:
+            if type(node_module) not in hooks_dict:
                 mod_name = node.target.replace('_', '.')
                 new_module = replace_operations(
                     node_module, new_operations, skip_modules_list
                 )
-                gm.add_submodule(mod_name, new_module)
+                fx_model.add_submodule(mod_name, new_module)
 
                 with graph.inserting_after(node):
                     new_node = graph.call_module(
@@ -66,11 +59,15 @@ def replace_operations(module: torch.nn.Module,
                     )
                     node.replace_all_uses_with(new_node)
                     graph.erase_node(node)
+            else:
+                node_module.register_forward_hook(
+                    hooks_dict[type(node_module)]
+                )
 
     graph.lint()
-    gm.recompile()
+    fx_model.recompile()
 
-    return gm
+    return fx_model
 
 
 def prepare_parameters(cont_parameters):
@@ -93,19 +90,19 @@ def prepare_parameters(cont_parameters):
 
 
 def build_groups(model, sample_shape, cont_parameters):
-
     tracing_model = replace_operations(model)
-    all_grids = prepare_parameters(cont_parameters)
+    all_groups = prepare_parameters(cont_parameters)
     device = next(iter(model.parameters())).device
     x = torch.rand(sample_shape).to(device)
     tracing_model(x)
     remove_all_hooks(tracing_model)
     del tracing_model
-    grids = [
-        g for g in all_grids if len(g['params']) != 0
+    groups = [
+        group for group in all_groups
+        if len(group['params']) != 0
     ]
 
-    return grids
+    return groups
 
 
 if __name__ == '__main__':
