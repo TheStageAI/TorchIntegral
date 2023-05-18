e4a5c8ede1500b7fbbc211d7d3a78a448010ba0b
b1n0 <azim4fs@gmail.com>
2023-05-19 01:29:55 +0300

    added default hooks

--- torch_integral/graph/default_operations.py
+++ torch_integral/graph/default_operations.py
@@ -13,10 +13,17 @@ DEFAULT_OPERATIONS = {
     torch.conv2d: conv_linear_decorator(torch.conv2d),
     torch.conv3d: conv_linear_decorator(torch.conv3d),
     torch._C._nn.linear: conv_linear_decorator(torch._C._nn.linear),
+    torch.nn.functional.batch_norm: batch_norm,
     'mean': aggregation_decorator(torch.mean),
     'sum': aggregation_decorator(torch.sum),
     'view': reshape,
     'reshape': reshape,
     'mul': operators_decorator(operator.mul),
     'add': operators_decorator(operator.add),
+}
+
+DEFAULT_HOOKS = {
+    torch.nn.BatchNorm1d: neutral_hook,
+    torch.nn.BatchNorm2d: neutral_hook,
+    torch.nn.BatchNorm3d: neutral_hook,
 }
