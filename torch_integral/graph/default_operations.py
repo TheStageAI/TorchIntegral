9f52626a1d9fe5d30165a9ec2285578b900f0754
b1n0 <azim4fs@gmail.com>
2023-05-19 14:45:50 +0300

    add replace_operations function

--- torch_integral/graph/default_operations.py
+++ torch_integral/graph/default_operations.py
@@ -1,5 +1,7 @@
 import operator
 from .operations import *
+from .operations import neutral_decorator
+from ..utils import get_attr_by_name
 
 
 DEFAULT_OPERATIONS = {
@@ -27,3 +29,68 @@ DEFAULT_HOOKS = {
     torch.nn.BatchNorm2d: neutral_hook,
     torch.nn.BatchNorm3d: neutral_hook,
 }
+
+
+def replace_operations(module: torch.nn.Module,
+                       new_operations=None) -> torch.nn.Module:
+
+    fx_model = torch.fx.symbolic_trace(module)
+    modules = dict(fx_model.named_modules())
+    graph = fx_model.graph
+    operations = DEFAULT_OPERATIONS.copy()
+    hooks_dict = DEFAULT_HOOKS.copy()
+    nodes = list(graph.nodes)
+
+    if new_operations is not None:
+        operations.update(new_operations)
+
+    for node in nodes:
+        if node.op == 'call_function':
+            if node.target in operations:
+                node.target = operations[node.target]
+            else:
+                node.target = neutral_decorator(node.target)
+
+        elif node.op == 'call_method' and node.target in operations:
+            func = operations[node.target]
+            args = node.args
+
+            if node.target == 'view' or node.target == 'reshape':
+                args = tuple([*node.args, node.target])
+
+            elif node.target == 'mean' or node.target == 'sum':
+                args = node.args[1:]
+
+            with graph.inserting_after(node):
+                new_node = graph.call_function(
+                    func, args, node.kwargs
+                )
+                node.replace_all_uses_with(new_node)
+                graph.erase_node(node)
+
+        elif node.op == 'call_module':
+            # node_module = modules[node.target]
+            node_module = get_attr_by_name(module, node.target)
+
+            if type(node_module) not in hooks_dict:
+                mod_name = node.target.replace('_', '.')
+                new_module = replace_operations(
+                    node_module, new_operations,
+                )
+                fx_model.add_submodule(mod_name, new_module)
+
+                with graph.inserting_after(node):
+                    new_node = graph.call_module(
+                        mod_name, node.args, node.kwargs
+                    )
+                    node.replace_all_uses_with(new_node)
+                    graph.erase_node(node)
+            else:
+                node_module.register_forward_hook(
+                    hooks_dict[type(node_module)]
+                )
+
+    graph.lint()
+    fx_model.recompile()
+
+    return fx_model
