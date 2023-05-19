d14b0ea246e82859e8725999b325bbb33a99d223
b1n0 <azim4fs@gmail.com>
2023-05-19 20:39:23 +0300

    added verbose flag

--- torch_integral/permutation.py
+++ torch_integral/permutation.py
@@ -29,9 +29,10 @@ class BasePermutation:
 
 
 class NOptPermutation(BasePermutation):
-    def __init__(self, iters=100):
+    def __init__(self, iters=100, verbose=True):
         super(NOptPermutation, self).__init__()
         self.iters = iters
+        self.verbose = verbose
 
     def find_permutation(self, tensors, size):
         cities_names = [i for i in range(size)]
@@ -39,12 +40,15 @@ class NOptPermutation(BasePermutation):
         path_distance = Solver.calculate_path_dist(
             dist_mat, torch.arange(size)
         )
-        print(f'variation before permutation:', path_distance)
         route_finder = RouteFinder(
             dist_mat, cities_names, iterations=self.iters
         )
         best_distance, indices = route_finder.solve()
-        print(f'variation after permutation:', best_distance)
+
+        if self.verbose:
+            print(f'variation before permutation:', path_distance)
+            print(f'variation after permutation:', best_distance)
+
         device = tensors[0]['value'].device
         indices = torch.tensor(indices).to(device)
 
