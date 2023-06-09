73a8e08a3eefa35154df5118013863779e3b6b3b
b1n0 <azim4fs@gmail.com>
2023-06-09 16:41:26 +0300

    small fixes

--- torch_integral/permutation.py
+++ torch_integral/permutation.py
@@ -99,9 +99,7 @@ class NOptPermutation(BasePermutation):
 
 class NOptPermutationModified(NOptPermutation):
     def __init__(self, iters=100, verbose=True):
-        super(NOptPermutationModified, self).__init__(
-            iters, verbose
-        )
+        super(NOptPermutationModified, self).__init__(iters, verbose)
 
     def choose_tensors(self, tensors):
         out = [
@@ -115,132 +113,91 @@ class NOptPermutationModified(NOptPermutation):
         return out
 
 
-class VarianceOptimizer:
-    def __init__(self, num_iters=3000, verbose=True):
-        self.num_iters = num_iters
-        self.verbose = verbose
-        self.eps = 1e-8
-
-    def total_variance(self, tensors):
-        total_var = 0.
-
-        for t in tensors:
-            tensor = t['value']
-            dim = t['dim']
-            tensor = tensor.transpose(dim, 0)
-            diff = (tensor[1:] - tensor[:-1]).abs().mean()
-            total_var = total_var + diff
-
-        return total_var
-
-    def choose_tensors(self, tensors):
-        out = [
-            t for t in tensors if 'bias' not in t['name']
-        ]
-
-        if len(out) == 0:
-            out = tensors
-
-        return out
-
-    def find_scale_vector(self, tensors, size):
-        tensors = self.choose_tensors(tensors)
-        device = tensors[0]['value'].device
-        best_vector = torch.ones(size, device=device)
-        best_var = self.total_variance(tensors)
-
-        if self.verbose:
-            print('before scaling: ', best_var)
-
-        for i in range(self.num_iters):
-            rand_vector = torch.rand(size, device=device)
-            scaled_tensors = []
-
-            for t in tensors:
-                tensor = t['value']
-                dim = t['dim']
-                shape = [1] * tensor.ndim
-                shape[dim] = rand_vector.shape[0]
-                scale = rand_vector.view(shape)
-
-                if dim == 0:
-                    tensor = tensor * scale
-                else:
-                    tensor = tensor / scale
-
-                scaled_tensors.append({
-                    'dim': dim, 'value': tensor
-                })
-
-            total_var = self.total_variance(scaled_tensors)
-
-            if total_var < best_var:
-                best_var = total_var
-                best_vector = rand_vector
-
-        if self.verbose:
-            print('before scaling: ', best_var)
-
-        return best_vector
-
-    # def find_scale_vector(self, tensors, size):
-    #     tensors = self.choose_tensors(tensors)
-    #     scale_vect = torch.nn.Parameter(
-    #         torch.ones(size).to(tensors[0]['value'].device)
-    #     )
-    #     opt = torch.optim.Adam(
-    #         [scale_vect], lr=1e-3, weight_decay=0.
-    #     )
-    #
-    #     if self.verbose:
-    #         print(
-    #             'before scaling: ',
-    #             self.total_variance(tensors)
-    #         )
-    #
-    #     for i in range(self.num_iters):
-    #         scaled_tensors = []
-    #
-    #         for t in tensors:
-    #             tensor = t['value']
-    #             dim = t['dim']
-    #             shape = [1] * tensor.ndim
-    #             shape[dim] = scale_vect.shape[0]
-    #             scale = scale_vect.view(shape)
-    #
-    #             if dim == 0:
-    #                 tensor = tensor * scale
-    #             else:
-    #                 tensor = tensor / scale
-    #
-    #             scaled_tensors.append({
-    #                 'dim': dim, 'value': tensor
-    #             })
-    #
-    #         total_var = self.total_variance(scaled_tensors)
-    #         total_var.backward()
-    #         opt.step()
-    #         opt.zero_grad()
-    #
-    #     if self.verbose:
-    #         print(
-    #             'after scaling: ',
-    #             self.total_variance(scaled_tensors)
-    #         )
-    #
-    #     return scale_vect
-
-    def __call__(self, tensors, size):
-        scale_vector = self.find_scale_vector(tensors, size)
-
-        for t in tensors:
-            dim = t['dim']
-            tensor = t['value']
-            shape = [1] * tensor.ndim
-            shape[dim] = scale_vector.shape[0]
-            scale = scale_vector.view(shape)
-
-            if dim == 0:
-                tensor.data = tensor * scale
-            elif dim == 1:
-                tensor.data = tensor / scale
+# class VariationOptimizer:
+#     def __init__(self, num_iters=3000,
+#                  learning_rate=1e-3,
+#                  verbose=True):
+#
+#         self.num_iters = num_iters
+#         self.verbose = verbose
+#         self.learning_rate = learning_rate
+#         self.eps = 1e-8
+#
+#     def total_variance(self, tensors):
+#         total_var = 0.
+#
+#         for t in tensors:
+#             tensor = t['value']
+#             dim = t['dim']
+#             tensor = tensor.transpose(dim, 0)
+#             diff = (tensor[1:] - tensor[:-1]).abs().mean()
+#             total_var = total_var + diff
+#
+#         return total_var
+#
+#     def choose_tensors(self, tensors):
+#         out = [
+#             t for t in tensors if 'bias' not in t['name']
+#         ]
+#
+#         if len(out) == 0:
+#             out = tensors
+#
+#         return out
+#
+#     def find_scale_vector(self, tensors, size):
+#         # tensors = self.choose_tensors(tensors)
+#         device = tensors[0]['value'].device
+#         scale_vect = 2. * (torch.rand(size) > 0.5) - 1.
+#         # scale_vect = torch.ones(size)
+#         scale_vect = torch.nn.Parameter(scale_vect.to(device))
+#         opt = torch.optim.Adam(
+#             [scale_vect], lr=self.learning_rate, weight_decay=0.
+#         )
+#
+#         if self.verbose:
+#             print('before scaling: ', self.total_variance(tensors))
+#
+#         for i in range(self.num_iters):
+#             scaled_tensors = []
+#
+#             for t in tensors:
+#                 tensor = t['value']
+#                 dim = t['dim']
+#                 shape = [1] * tensor.ndim
+#                 shape[dim] = scale_vect.shape[0]
+#                 scale = scale_vect.view(shape)
+#
+#                 if dim == 0:
+#                     tensor = tensor * scale
+#                 else:
+#                     tensor = tensor / scale
+#
+#                 scaled_tensors.append({
+#                     'dim': dim, 'value': tensor
+#                 })
+#
+#             total_var = self.total_variance(scaled_tensors)
+#             total_var.backward()
+#             opt.step()
+#             opt.zero_grad()
+#
+#         if self.verbose:
+#             print('after scaling: ', self.total_variance(scaled_tensors))
+#
+#         return scale_vect
+#
+#     def __call__(self, tensors, size):
+#         scale_vector = self.find_scale_vector(tensors, size)
+#
+#         for t in tensors:
+#             dim = t['dim']
+#             tensor = t['value']
+#             shape = [1] * tensor.ndim
+#             shape[dim] = scale_vector.shape[0]
+#             scale = scale_vector.view(shape)
+#
+#             if dim == 0:
+#                 tensor.data = tensor.data * scale
+#             else:
+#                 tensor.data = tensor.data / scale
