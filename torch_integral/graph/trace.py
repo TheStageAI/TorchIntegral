92260695485438790186e53732882a3941cd5ce1
b1n0 <azim4fs@gmail.com>
2023-06-12 16:01:32 +0300

    added black list names and dims in Tracer

--- torch_integral/graph/trace.py
+++ torch_integral/graph/trace.py
@@ -2,15 +2,21 @@ import torch
 from .operations import replace_operations
 from .integral_group import IntegralGroup
 from ..utils import remove_all_hooks
-    
-    
+
+
 class Tracer:
     def __init__(self, model,
-                 sample_shape,
-                 continuous_dims):
+                 example_input,
+                 continuous_dims,
+                 black_list_dims=None):
+
+        if black_list_dims is not None:
+            self.black_list_dims = black_list_dims
+        else:
+            self.black_list_dims = {}
 
         self.continuous_dims = continuous_dims
-        self.sample_shape = sample_shape
+        self.example_input = example_input
         self.model = model
         self.groups = None
 
@@ -36,13 +42,22 @@ class Tracer:
         delete_indices = []
 
         for i, group in enumerate(self.groups):
-            group.clear_tensors()
             delete_group = True
 
             for p in group.params:
-                if p['name'] in self.continuous_dims:
-                    if p['dim'] in self.continuous_dims[p['name']]:
-                        delete_group = False
+                if p['name'] in self.continuous_dims and \
+                        p['dim'] in self.continuous_dims[p['name']]:
+                    delete_group = False
+
+                if p['name'] in self.black_list_dims and \
+                        p['dim'] in self.black_list_dims[p['name']]:
+
+                    for d in group.params:
+                        if d['name'] in self.continuous_dims:
+                            self.continuous_dims[d['name']].pop(d['dim'])
+
+                    delete_group = True
+                    break
 
             if delete_group:
                 delete_indices.append(i)
@@ -60,7 +75,6 @@ class Tracer:
             group for i, group in enumerate(self.groups)
             if i not in delete_indices
         ]
-
         parents = set()
 
         for group in self.groups:
@@ -78,13 +92,17 @@ class Tracer:
         tracing_model = replace_operations(self.model)
         self._preprocess_parameters()
         device = next(iter(self.model.parameters())).device
-        x = torch.rand(self.sample_shape).to(device)
+
+        if type(self.example_input) == torch.Tensor:
+            x = self.example_input.to(device)
+        else:
+            x = torch.rand(self.example_input).to(device)
+
         tracing_model(x)
         remove_all_hooks(tracing_model)
         del tracing_model
         self.groups = [
-            group for group in self.groups
-            if len(group.params) != 0
+            group for group in self.groups if len(group.params) != 0
         ]
         parents = self._postprocess_groups()
 
